44369192b9dbc6c0f1d2b31585fc2c14ba07fa30
Faber222 <faber.b@aluno.ifsc.edu.br>
2021-03-29 20:35:20 -0300

    faber

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const server = require("http").Server(app);
 
 const PORT = 3500;
 
-app.use(express.static("../"));
+app.use(express.static("../public/index.html"));
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
